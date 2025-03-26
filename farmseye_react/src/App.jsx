4fd7d003cb04c8d04b51b7c6213460e3b91c147b
pseudopamine <jinseonglae@gmail.com>
2025-03-26 15:22:08 +0900

    250326 메인레이아웃 설정

--- farmseye_react/src/App.jsx
+++ farmseye_react/src/App.jsx
@@ -2,13 +2,21 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import UserLayout from './components/UserLayout'
 import AdminLayout from './components/AdminLayout'
+import MainLayout from './components/MainLayout'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div className='container'>
+      <Header />
       
       <Routes>
+        <Route path="/main" element={ <MainLayout /> }>
+          <Route path='' element={ <div>로그인페이지</div>}/>
+        </Route>
+          
+        
 
         <Route path="/" element={ <UserLayout/> } >
           <Route path="" element={ <div>메인 유저 페이지</div> } />
---
+++ farmseye_react/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div>이곳은 헤더영역입니다.</div>
+    </>
+  );
+};
+
+export default Header;
