4b1133e34f52568e0134babfb4cd94d7f4c7bb0f
pseudopamine <jinseonglae@gmail.com>
2025-03-26 15:52:14 +0900

    250326 메인레이아웃 제작

--- farmseye_react/src/App.jsx
+++ farmseye_react/src/App.jsx
@@ -2,25 +2,30 @@ import { data, Route, Routes } from 'react-router-dom'
 import './App.css'
 import UserLayout from './components/UserLayout'
 import AdminLayout from './components/AdminLayout'
-import TemperatureTest from './components/TemperatureTest'
+// import TemperatureTest from './components/TemperatureTest'
+import MainLayout from './components/MainLayout'
 
 function App() {
 
   return (
     <div className='container'>
-      <TemperatureTest/>
+      {/* <TemperatureTest/> */}
 
-      {/* <Routes>
 
-        <Route path="/" element={ <UserLayout/> } >
+      <Routes>
+        <Route path='/main' element={ <MainLayout/> }>
+          <Route path='' element={ <div>로그인 페이지</div> }/>
+        </Route>
+
+        {/* <Route path="/" element={ <UserLayout/> } >
           <Route path="" element={ <div>메인 유저 페이지</div> } />
         </Route>
 
         <Route path="/admin" element={ <AdminLayout/> } >
           <Route path="" element={ <div>관리자 페이지</div> } />
-        </Route>
+        </Route> */}
 
-      </Routes> */}
+      </Routes>
 
     </div>
   )
